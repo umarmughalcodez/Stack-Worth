ffd7e9d72bcbd246eec6fd911ff30344ba7f3bbc
xcorpion <umarmughal255372@gmail.com>
2025-03-21 02:38:55 +0500

    Finally Deployed Again!

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -18,9 +18,8 @@ export async function POST(req: Request) {
     const data = await geminiRes.json();
     return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json(
-      { error: "Failed to fetch response from Google Gemini" },
-      { status: 500 }
-    );
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error?.message }, { status: 500 });
+    }
   }
 }
